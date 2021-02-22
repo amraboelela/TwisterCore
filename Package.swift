5a871ae037401c35d161d74e4bd74518d7264274
Amr Aboelela <amraboelela@gmail.com>
2021-02-22 11:41:02 -0800

    bitcoinmain

--- Package.swift
+++ Package.swift
@@ -7,20 +7,22 @@ let package = Package(
     name: "SwiftTwisterCore",
     products: [
         .library(name: "SwiftTwisterCore", targets: ["CTwisterCore"]),
-        //.library(name: "twisterbridge", targets: ["twisterbridge"])
     ],
     dependencies: [
-        .package(url: "https://github.com/amraboelela/boost", .branch("master")),
+        .package(url: "https://github.com/amraboelela/jsoncpp", .branch("master")),
         .package(url: "https://github.com/amraboelela/openssl", .branch("master")),
-        //.package(url: "https://github.com/amraboelela/leveldb", .branch("master")),
+        .package(url: "https://github.com/amraboelela/leveldb", .branch("master")),
+        .package(url: "https://github.com/amraboelela/boost", .branch("master")),
     ],
     targets: [
         //.target(name: "twistercore", dependencies: ["boost", "openssl", "leveldb", "memenv"]),
         //.target(name: "memenv", dependencies: ["leveldb"]),
         //.target(name: "SwiftTwisterCore", dependencies: ["CTwisterCore"]),
         .target(name: "CTwisterCore", dependencies: ["twistercore"]),
-        .target(name: "twistercore", dependencies: ["twistercore_macos"]),
-        .target(name: "twistercore_macos", dependencies: ["boost", "openssl"])
+        .target(name: "twistercore", dependencies: ["bitcoinleveldb", "jsoncpp", "openssl"]),
+        //.target(name: "twistertorrent", dependencies: ["bitcoinleveldb"]),
+        .target(name: "bitcoinleveldb", dependencies: ["leveldb", "boost"]),
+        //.target(name: "twistercore_macos", dependencies: ["boost", "openssl"])
         //.target(name: "twistercore", dependencies: ["boost", "openssl", "leveldb"]),
         //.testTarget(name: "SwiftTwisterCoreTests", dependencies: ["SwiftTwisterCore"]),
     ]
